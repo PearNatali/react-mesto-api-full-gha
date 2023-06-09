2e9a4a4927074b69b615a0ff13f6711a14f00655
Natalia Grusha <gruscha.natascha2017@yandex.ru>
2023-06-09 20:58:18 +0300

    fix: вернула обратно все изменения

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,15 +4,17 @@ import {Route, Routes, Navigate, useNavigate} from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import api from "../utils/Api";
 import ImagePopup from './ImagePopup';
+import InfoTooltip from "./InfoTooltip";
 import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
-import AddPlacePopup from "./AddPlacePopup";
 import ProtectedRoute from "./ProtectedRoute";
-import InfoTooltip from "./InfoTooltip";
+import AddPlacePopup from "./AddPlacePopup";
 import Login from "./Login";
 import Register from "./Register";
+import * as auth from '../utils/auth';
+
+import api from "../utils/Api";
 
 import { CurrentUserContext } from '../contexts/CurrentUserContext'
 import {CardsContext} from '../contexts/CardsContext'
@@ -23,50 +25,32 @@ function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
   const [isPopupPictureOpen, setIsPopupPictureOpen] = useState(false);
   const [isInfoTooltipOpen, setIsInfoTooltipOpen] = useState(false);
-  const [isTooltipSuccess, setIsTooltipSuccess] = useState(false);
+  const [isRegisterSuccess, setIsRegisterSuccess] = useState(false);
   const [selectedCard, setSelectedCard] = useState({ });
   const [currentUser, setCurrentUser] = useState({ });
   const [cards, setCards] = useState([]);
   const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
 
   const navigate = useNavigate();
 
-  const handleRegister = (isRegisterSuccess) => {
-    setIsInfoTooltipOpen(true);
-    setIsTooltipSuccess(isRegisterSuccess);
-  }
-
-  const handleLogin = () => {
-    const token = localStorage.getItem('token');
-        if (token) {
-            api.setHeaders(token);
-            Promise.all([api.getUserInfo(), api.getInitialCards()])
-                .then(([userData, cards]) => {
-                    setCurrentUser(userData);
-                    setCards(cards);
-                    setLoggedIn(true);
-                    navigate("/main", {replace: true});
-                })
-                .catch(err => console.log(err))
-        }
-  }
+    useEffect(() => {
+      handleTokenCheck()
+     
+    }, [])
   
-  const handleLoginFail = () => {
-    setIsInfoTooltipOpen(true);
-    setIsTooltipSuccess(false);
-  }
-
-  const handleSignOut = () => {
-    setLoggedIn(false);
-    setCurrentUser({});
-    setCards([]);
-    localStorage.removeItem('token');
-  }
-
   useEffect(() => {
-    handleLogin()
-  }, [])
-  
+      if (loggedIn) {
+      Promise.all([api.getUserInfo(), api.getInitialCards()])
+            .then(([userData, cards]) => {
+              setCurrentUser(userData);
+              setCards(cards)
+            })
+            .catch(err => console.log(err))
+      }
+      }, [loggedIn])
+
+
   const handleEditProfileClick = () => {
     setIsEditProfilePopupOpen(true);
   };
@@ -152,13 +136,76 @@ function App() {
         .catch(err => console.log(err))
   }
 
+  const handleRegister = (email, password) => {
+    if (!email || !password){
+      return;
+  }
+    auth.register(email, password)
+      .then((res) => {
+        setIsInfoTooltipOpen(true);
+        if(res) {
+          navigate('/sign-in', {replace: true});
+          setIsRegisterSuccess(true);
+        }
+      })
+      .catch(() => {
+        setIsInfoTooltipOpen(true);
+        setIsRegisterSuccess(false);
+      });
+  }
+
+  const handleLogin = (email, password) => {
+    if (!email || !password){
+      return;
+  }
+    auth.authorize(email, password)
+      .then((data) => {
+        if(data && data.token) {
+          setLoggedIn(true);
+          setEmail(email);
+          localStorage.setItem('token', data.token);
+          navigate('/main' , {replace: true});
+        } else {
+          alert ("Неверный логин или пароль")
+        }
+      })
+      .catch(() => {
+        setIsInfoTooltipOpen(true);
+        setIsRegisterSuccess(true);
+        setLoggedIn(false);
+      })
+  }
+  
+  const handleSignOut = () => {
+    setLoggedIn(false);
+    setEmail(' ');
+    localStorage.removeItem('token');
+  }
+
+ const handleTokenCheck = () => {
+      const token = localStorage.getItem('token');
+      if (localStorage.getItem('token')) {
+      auth.checkToken(token)
+        .then((res) => {
+          if (res) {
+            setLoggedIn(true);
+            setEmail(res.data.email);
+            navigate('/main', {replace: true})
+          }
+        })
+        .catch(err => console.log(err))
+    }
+  }
+
   return (
     <CardsContext.Provider value={cards}>
       <CurrentUserContext.Provider value={currentUser}>
         <div className="page">
           <div className="page__content">
-          <Header onSignOut={handleSignOut} />
+          <Header email={email} onSignOut={handleSignOut} />
           <Routes>
+            <Route path='/sign-up' element={<Register handleRegister={handleRegister}/>} />
+            <Route path='/sign-in' element={<Login handleLogin={handleLogin}/>} />
             <Route 
               path='/' element={loggedIn ? <Navigate to='/main' replace/> : <Navigate to='/sign-in' replace/>}/>
             <Route 
@@ -166,6 +213,7 @@ function App() {
               element={
                 <ProtectedRoute
                   element={Main}
+                  cards={cards}
                   loggedIn={loggedIn}
                   onEditProfile={handleEditProfileClick}
                   onAddPlace={handleEditPlaceClick}
@@ -174,8 +222,6 @@ function App() {
                   onLikeClick={handleCardLike}
                   onDeleteClick={handleCardDelete}/>
               }/>
-            <Route path='/sign-up' element={<Register handleRegister={handleRegister}/>} />
-            <Route path='/sign-in' element={<Login handleLoginFail={handleLoginFail} handleLogin={handleLogin}/>} />
           </Routes>
           <Footer/>
           <EditProfilePopup
@@ -202,7 +248,7 @@ function App() {
             isOpen={isInfoTooltipOpen}
             onButtonClose={closeAllPopups}
             onOverlayClose={closePopupByOverlayClick}
-            isTooltipSuccess={isTooltipSuccess}/>
+            isRegisterSuccess={isRegisterSuccess}/>
           </div>
         </div>
       </CurrentUserContext.Provider>
