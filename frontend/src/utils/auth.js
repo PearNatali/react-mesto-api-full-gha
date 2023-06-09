326a683699bb4353c82596958c6832e4345cca6d
Natalia Grusha <gruscha.natascha2017@yandex.ru>
2023-06-09 15:51:37 +0300

    fix: url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://api.pearnatali.nomoredomains.rocks';
+export const baseUrl = 'https://auth.nomoreparties.co';
 
 export const checkRequest = (res) => {
     if (res.ok) {
