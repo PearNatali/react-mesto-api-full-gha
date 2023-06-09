d4c18c5aa84f540863340fc395fa0be54d64092a
Natalia Grusha <gruscha.natascha2017@yandex.ru>
2023-06-09 21:38:40 +0300

    fix: repeat

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -32,18 +32,11 @@ export const authorize = (email, password) => {
             },
             body: JSON.stringify({ email, password })
         })
-        .then((res) => {return checkRequest(res)})
-}
-
-export const checkToken = (token) => {
-    const newUrl = baseUrl + '/users/me';
-        return fetch(newUrl, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
+        .then(res => checkRequest(res))
+        .then((data) => {
+            if (data.token){
+                localStorage.setItem('token', data.token);
+                return data;
             }
         })
-        .then((res) => {return checkRequest(res)})
 }
