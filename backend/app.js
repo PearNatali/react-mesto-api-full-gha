c0784bba1c67cdb55afc3d084bfc24bdfc853239
Natalia Grusha <gruscha.natascha2017@yandex.ru>
2023-06-10 21:47:13 +0300

    Поставила скобки cors

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors);
+app.use(cors());
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
